14a5a25c89004ce93155636bedff69c4532796d9
Aamst258 <ameerahmadtopiwale1@gmail.com>
2022-11-16 21:11:04 +0530

    successfully issue resolved

---
+++ Registration form/index.js
@@ -0,0 +1,14 @@
+//   Accessing entered values 
+   
+  
+//    displaying entered values
+    
+  
+function  display(){
+    let fname = document.getElementById("firstname").value; 
+    let full_name = document.getElementById("name") ;
+    full_name.innerHTML = fname ;
+    
+}
+    
+    
